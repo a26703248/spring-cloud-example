834be70971458fcbca21cadf148a13b921312eb0
李澔澤 <a0909007892@gmail.com>
2023-09-09 14:11:53 +0800

    add security server

---
+++ spring-security-oauth-server/src/main/java/com/howard7892/springsecurityoauthserver/SpringSecurityOauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.howard7892.springsecurityoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthServerApplication.class, args);
+	}
+
+}
---
+++ spring-security-oauth-server/src/test/java/com/howard7892/springsecurityoauthserver/SpringSecurityOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howard7892.springsecurityoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
